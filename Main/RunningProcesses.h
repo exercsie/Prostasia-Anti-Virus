0eda246b7f2c89127586592a2259ff80a8a5058d
exercsie <hammondtristan62@gmail.com>
2026-05-18 11:14:01 +1000

    Refractored files

--- Main/RunningProcesses.h
+++ Main/RunningProcesses.h
@@ -16,3 +16,70 @@ public:
     void killProcess(const std::string &process);
     void runOnStartup(const std::string &programName, const std::string &programPath);
 };
+
+std::vector<std::string> RunningProcesses::getRunningProcesses() {
+    std::vector<std::string> processes;
+
+    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+    if(snapshot == INVALID_HANDLE_VALUE) {
+        return processes;
+    }
+
+    PROCESSENTRY32 processEntry;
+    processEntry.dwSize = sizeof(processEntry);
+
+    if(Process32First(snapshot, &processEntry)) {
+        do {
+            processes.push_back(processEntry.szExeFile);
+        } while(Process32Next(snapshot, &processEntry));
+    }
+
+    CloseHandle(snapshot);
+
+    // dispaly all processes
+    /*for(const std::string &process : processes) {
+        std::cout << process << std::endl;
+    }*/
+
+    return processes;
+}
+
+DWORD RunningProcesses::getPID(const std::string &process) {
+    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+    if(snapshot == INVALID_HANDLE_VALUE) {
+        return 0;
+    }
+
+    PROCESSENTRY32 processEntry;
+    processEntry.dwSize = sizeof(processEntry);
+
+    if(Process32First(snapshot, &processEntry)) {
+        do {
+            if(process == processEntry.szExeFile) {
+                CloseHandle(snapshot);
+                return processEntry.th32ProcessID;
+            }
+        } while(Process32Next(snapshot, &processEntry));
+    }
+
+    CloseHandle(snapshot);
+    return 0;
+}
+
+void RunningProcesses::killProcess(const std::string &process) {
+    DWORD pid = getPID(process);
+
+    HANDLE p = OpenProcess(PROCESS_TERMINATE, FALSE, pid);
+    TerminateProcess(p, 0);
+    CloseHandle(p);
+}
+
+void RunningProcesses::runOnStartup(const std::string &programName, const std::string &programPath) {
+    HKEY key;
+
+    RegOpenKeyExA(HKEY_CURRENT_USER, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_SET_VALUE, &key);
+    RegSetValueExA(key, programName.c_str(), 0, REG_SZ, (BYTE*)programPath.c_str(), programPath.size() + 1);
+    RegCloseKey(key);
+}
--- Main/Vector.h
+++ Main/Vector.h
@@ -13,3 +13,14 @@ class Vector {
 public:
     std::vector<std::string> removeFromVector(std::vector<std::string> &v, std::string &key);
 };
+
+std::vector<std::string> Vector::removeFromVector(std::vector<std::string> &v, std::string &key) {
+    for(int i = 0; i < v.size(); i++) {
+        if(key == v.at(i)) {
+            v.erase(v.begin() + i);
+            return v;
+        }
+    }
+
+    return v;
+}
